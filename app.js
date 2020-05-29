5a366651896f97b156479650e1b3607796692a7d
pikktorr <viktor.rozgonyi@gmail.com>
2020-05-29 17:16:48 +0200

    fetch api testing

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
-const capitalize = string => {
-  return string[0].toUpperCase() + string.slice(1);
+const fetch = require("node-fetch");
+
+const googleDatabase = [
+  "cats.com",
+  "souprecipes.com",
+  "flowers.com",
+  "playstation.com",
+  "catpictures.com",
+  "myfavouritecats.com",
+  "catnip.com",
+];
+
+const googleSearch = (database, searchInput) => {
+  const matches = database.filter((website) => {
+    return website.includes(searchInput);
+  });
+  // only return the first 3 results
+  return matches.length > 3 ? matches.slice(0, 3) : matches;
 };
 
-const add = (a, b) => a + b;
-const subtract = (a, b) => a - b;
-const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
-
-module.exports = {
-  capitalize,
-  add,
-  subtract,
-  multiply,
-  divide
+const getData = (fetch) => {
+  return fetch("https://acnhapi.com/v1/art")
+    .then((response) => response.json())
+    .then((result) => {
+      Object.entries(result).map((data) => {
+        console.log(data[1]);
+      });
+    });
 };
+getData(fetch);
+
+module.exports = { googleSearch, getData };
--- app.test.js
+++ app.test.js
@@ -1,21 +1,31 @@
 const fn = require("./app.js");
 
-test("first", () => {
-  expect(fn.capitalize("fitty")).toBe("Fitty");
+const dbMock = [
+  "twitter.com",
+  "nintendo.com",
+  "nintendoeurope.com",
+  "playstation.com",
+  "",
+];
+
+describe("google Search", () => {
+  it("searching Google", () => {
+    // there is no "nope" in the db -> array will be empty
+    expect(fn.googleSearch(dbMock, "nope")).toEqual([]);
+    expect(fn.googleSearch(dbMock, "ninten")).toEqual([
+      "nintendo.com",
+      "nintendoeurope.com",
+    ]);
+  });
+
+  it("handling undefined and null", () => {
+    expect(fn.googleSearch(dbMock, undefined)).toEqual([]);
+    expect(fn.googleSearch(dbMock, null)).toEqual([]);
+  });
+
+  it("doesn't return more than 3 matches", () => {
+    expect(fn.googleSearch(dbMock, ".com").length).toEqual(3);
+  });
 });
 
-test("add", () => {
-  expect(fn.add(2, 2)).toBe(4);
-});
-
-test("subtract", () => {
-  expect(fn.subtract(2, 2)).toBe(0);
-});
-
-test("multiply", () => {
-  expect(fn.multiply(2, 2)).toBe(4);
-});
-
-test("divide", () => {
-  expect(fn.divide(2, 2)).toBe(1);
-});
+describe("fetching API tests", () => {});
